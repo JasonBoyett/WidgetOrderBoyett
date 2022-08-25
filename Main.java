6a94df9bf42cb2483166232d83352b9637cf649e
JasonBoyett <jason.boyett@gmail.com>
2022-08-24 21:37:40 -0500

    almost done need to round off to two decimals

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("Hello!\nHow many widgets would you like to purchase today?\n");
         Scanner input = new Scanner(System.in);
         int numberOfWidgets = input.nextInt();
-        System.out.println("Of course! Here is your reciept, have a wounderful day!\n\n");
+        System.out.println("Of course! H55ere is your reciept, have a wounderful day!\n\n");
         Receipt userReceipt = new Receipt(numberOfWidgets, userWidget);
         userReceipt.printReceipt();
     }
--- Receipt.java
+++ Receipt.java
@@ -4,21 +4,22 @@ public class Receipt {
     private double grandTotal;
     private double shipping;
     private Widget aWidget;
+    private int numberPurchased;
 
     public Receipt(int numberOfWidgets, Widget whatWidget) {
         this.subTotal = (((double) (whatWidget.getPriceInCents()) / 100) * numberOfWidgets);
-        this.shipping = (((double) (numberOfWidgets * shipping)) / 100);
+        this.shipping = (((double) (numberOfWidgets * whatWidget.getCentsToShip())) / 100);
         this.taxTotal = (this.subTotal + this.shipping) * whatWidget.getTaxPercentage();
         this.grandTotal = this.subTotal + this.shipping + this.taxTotal;
         this.aWidget = whatWidget;
+        this.numberPurchased = numberOfWidgets;
     }
 
     public void printReceipt() {
         System.out.println("Thank you for your purchase!\n\nsubtotal: $" + this.subTotal + "\n");
         System.out.println("Tax: $" + this.taxTotal + " tax is added to your puchase total including shipping\n");
         System.out.println("Shipping: $" + this.shipping + "\n");
-        System.out.println("Shipping: $" + this.shipping + "\n");
         System.out.println("Total: $" + this.grandTotal + "\t");
-        System.out.println("Number of items purchased" + this.aWidget + "\n");
+        System.out.println("Number of items purchased: " + this.numberPurchased + "\n");
     }
 }

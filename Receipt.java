37f8530b698d180a833ac097ab557352b4c7f83f
JasonBoyett <jason.boyett@gmail.com>
2022-08-26 08:23:56 -0500

    added rounding to two segnificant figures on receipt

--- Receipt.java
+++ Receipt.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Receipt {
     private double subTotal;
     private double taxTotal;
@@ -5,6 +7,20 @@ public class Receipt {
     private double shipping;
     private Widget aWidget;
     private int numberPurchased;
+    private final DecimalFormat roundOff = new DecimalFormat("0.00");
+
+    private DecimalFormat roundThis(double toRound) {
+        return roundThis(toRound);
+    }
+
+    public void printReceipt() {
+        System.out.println("Thank you for your purchase!\n\nsubtotal: $" + roundOff.format(this.subTotal) + "\n");
+        System.out.println(
+                "Tax: $" + roundOff.format(this.taxTotal) + " tax is added to your puchase total including shipping\n");
+        System.out.println("Shipping: $" + roundOff.format(this.shipping) + "\n");
+        System.out.println("Total: $" + roundOff.format(this.grandTotal) + "\t");
+        System.out.println("Number of items purchased: " + this.numberPurchased + "\n");
+    }
 
     public Receipt(int numberOfWidgets, Widget whatWidget) {
         this.subTotal = (((double) (whatWidget.getPriceInCents()) / 100) * numberOfWidgets);
@@ -15,11 +31,4 @@ public class Receipt {
         this.numberPurchased = numberOfWidgets;
     }
 
-    public void printReceipt() {
-        System.out.println("Thank you for your purchase!\n\nsubtotal: $" + this.subTotal + "\n");
-        System.out.println("Tax: $" + this.taxTotal + " tax is added to your puchase total including shipping\n");
-        System.out.println("Shipping: $" + this.shipping + "\n");
-        System.out.println("Total: $" + this.grandTotal + "\t");
-        System.out.println("Number of items purchased: " + this.numberPurchased + "\n");
-    }
 }
